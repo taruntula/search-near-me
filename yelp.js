ea1a1804f95cc21937e1a0dc7f599bcdc0a8500a
Kevin Ihm <kevinihm@gmail.com>
2019-09-17 12:10:00 -0700

    added yelp php for back-end access

--- yelp.js
+++ yelp.js
@@ -7,7 +7,7 @@ class Yelp {
   apiRequest() {
     var ajaxConfig = {
       dataType: 'json',
-      url: './yelp.php',
+      url: `./yelp.php`,
       method: 'get',
       headers: {
         Authorization: "Bearer lW5xlHMb8jKbQ5KV82iD5Y9uVhWK4h9Apiwn2B38wyo8ohRFHVmlAWm8QDPwkoqcDA7I6QY_VwiyRDec-GY3BSpk286o7Dnf7caOoPIuQMHfgaqowqB3TIBp9DCAXXYx",
@@ -19,6 +19,12 @@ class Yelp {
       },
       success: function(response) {
         console.log("Yelp:",response);
+        for (let i = 0; i < response.businesses.length; i++) {
+          var newDiv = $('<div>').addClass('foodBusiness');
+          var businessName = response.businesses[i].name;
+          newDiv.text(businessName);
+          $('body').append(newDiv);
+        }
       },
       error: function (response) {
         console.log("Yelp error:", response);
@@ -28,5 +34,5 @@ class Yelp {
   }
 }
 
-var ajax = new Yelp(92782, 10);2
+var ajax = new Yelp(92782, 16093);
 ajax.apiRequest();
