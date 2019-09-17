8b4ee81de49307c99a8aa6381ac922ef23ea0ae6
Kevin Ihm <kevinihm@gmail.com>
2019-09-17 13:37:47 -0700

    added get pokemon image method

--- pokemon.js
+++ pokemon.js
@@ -1,16 +1,17 @@
 class Pokemon{
-  constructor (name, image,temperature, weather){
-    this.result = null;
+  constructor (temperature, weather){
+    this.resultName = null;
+    this.resultImage = null;
     this.pokemonName = name;
-    this.pokemonImageAddress = image;
+    this.pokemonImage = null;
     this.temperature = temperature;
     this.pokemonBasedOnTemp = "";
     this.weather = weather;
-    this.weatherPokemon = this.checkWeather(this.weather);
+    this.weatherPokemon = null;
   }
 
 
-  getPokemonData(){
+getPokemonData(){
   var self = this;
   var ajaxConfigObject ={
     dataType: 'json',
@@ -30,47 +31,45 @@ class Pokemon{
 $.ajax(ajaxConfigObject);
 }
 
-checkTemperature(temperature) {
+getPokemonImage() {
+  var self = this;
+  var ajaxConfig = {
+    dataType: 'json',
+    url: 'https://pokeapi.co/api/v2/pokemon/' + this.pokemonName,
+    method: 'get',
+    success: function(result) {
+      console.log('pokemon Images:', result);
+      self.resultImage = result;
+      self.pokemonImage;
+    },
+    error: function(result) {
+      console.log('pokemonApi result:', result);
+    }
+  };
+  $.ajax(ajaxConfig);
+}
+
+checkTemperature (temperature) {
   if (temperature >= 0 && temperature <= 75){
-    this.pokemonBasedOnTemp = this.result.results[143];
+    this.pokemonBasedOnTemp = this.result.results[143].name;
+    this.pokemonName = this.result.results[143].name;
   }
   if(temperature >=75 && temperature <=80){
     this.pokemonBasedOnTemp = this.result.results[3].name;
+    this.pokemonName = this.result.results[3].name;
   } else if (temperature >= 81 && temperature <=86){
     this.pokemonBasedOnTemp = this.result.results[4].name;
+    this.pokemonName = this.result.results[4].name;
   } else if( temperature >=87 && temperature <= 110){
     this.pokemonBasedOnTemp = this.result.results[5].name;
+    this.pokemonName = this.result.results[5].name;
   }
 }
 
-checkWeather(weather){
+checkWeather (weather) {
   switch(weather){
-      case "tornado":
-        this.weatherPokemon = this.result.results[640].name;
-        break;
-      case "thunderstorm":
-        this.weatherPokemon = this.result.results[144].name;
-        break;
-      case "hail":
-        this.weatherPokemon = this.result.results[364].name;
-        break;
-      case "partly-cloudly-night":
-        this.weatherPokemon = this.result.results[683].name;
-        break;
-      case "partly-cloudly-day":
-        this.weatherPokemon = this.result.results[333].name;
-        break;
       case "cloudy":
         this.weatherPokemon = this.result.results[322].name;
-        break;
-      case "fog":
-        this.weatherPokemon = this.result.results[91].name;
-        break;
-      case "wind":
-        this.weatherPokemon = this.result.results[15].name;
-        break;
-      case "sleet":
-        this.weatherPokemon =  this.result.results[143].name;
         break;
       case "snow":
         this.weatherPokemon = this.result.results[451].name;
@@ -78,6 +77,11 @@ checkWeather(weather){
       case "rain":
         this.weatherPokemon = this.result.results[481].name;
         break;
+      case 'clear-day':
+        this.weatherPokemon = this.result.results[3].name;
+        break;
+      case 'thunderstorm':
+        this.weatherPokemon = this.result.results[3].name;
     }
   return this.weatherPokemon;
   }
