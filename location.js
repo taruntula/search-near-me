5c16c9900a8c4cfdd6cfecbaff32397ab0f2c121
Brena Patel <brenapatel@gmail.com>
2019-09-16 20:43:06 -0700

    ajax call  successful

--- location.js
+++ location.js
@@ -1,26 +1,29 @@
 class Location {
-  constructor(elementConfig){
-    this.elementConfig = elementConfig;
-    this.data = {};
+  constructor(ipAddress){
+    this.ipAddress = ipAddress;
+    this.longitude = this.longitude;
+    this.latitude = this.latitude;
+    this.zipCode = this.zipCode;
+
   }
   getLocation () {
 
     var ajaxConfigObject = {
       dataType: 'json',
+      //url: 'http://api.ipstack.com/' + this.ipAddress + '?access_key=1a1994f66f98738ef0680c97975ec64c',
       url: 'http://api.ipstack.com/174.76.22.227?access_key=1a1994f66f98738ef0680c97975ec64c',
       method: 'Get',
       data: {
-        api_key: '1a1994f66f98738ef0680c97975ec64c',
+        access_key: '1a1994f66f98738ef0680c97975ec64c',
 
       },
       success: function(response){
           console.log('response ', response)
-      }
+      },
       error: function(response){
         console.log('response ', response)
       }
     }
-
 
 
     $.ajax(ajaxConfigObject);
@@ -28,14 +31,6 @@ class Location {
   }
 
 
-
-
-
-
-
-
-
-
-
-
 }
+var getAjaxCall = new Location
+console.log(getAjaxCall.getLocation());
