3e5bb2058719a80862a47a93934957fae271172e
Kevin Ihm <kevinihm@gmail.com>
2019-09-17 16:22:29 -0700

    added main.js file

--- location.js
+++ location.js
@@ -3,10 +3,11 @@ class Location {
     this.longitude = this.longitude;
     this.latitude = this.latitude;
     this.zipCode = this.zipCode;
+    this.response = null;
 
   }
   getLocation () {
-
+    var self = this;
     var ajaxConfigObject = {
       dataType: 'json',
       //url: 'http://api.ipstack.com/' + this.ipAddress + '?access_key=1a1994f66f98738ef0680c97975ec64c',
@@ -17,12 +18,13 @@ class Location {
 
       },
       success: function(response){
+          this.response = response;
           console.log('response ', response)
-          this.longitude = response.longitude;
+          self.longitude = response.longitude;
           console.log('longitude', this.longitude);
-          this.latitude = response.latitude
+          self.latitude = response.latitude;
           console.log('latitude', this.latitude);
-          this.zipCode = response.zip
+          self.zipCode = response.zip;
           console.log('zipcode', this.zipCode);
       },
       error: function(response){
@@ -35,7 +37,11 @@ class Location {
 
   }
 
+  createObjects() {
+    var weather = new DarkskyApi(this.longitude, this.latitude);
+    return weather;
+  }
 
 }
-var getAjaxCall = new Location
+var getAjaxCall = new Location;
 console.log(getAjaxCall.getLocation());
