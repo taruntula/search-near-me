d1e45f8f5b33d398814bb5a2c76f7e4673d438ca
Michael Chang <michaelchangre@gmail.com>
2019-09-17 12:55:46 -0700

    added check temperature and weather

--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,12 @@
 class Pokemon{
-  constructor (name, image,temperature){
+  constructor (name, image,temperature, weather){
     this.result = null;
     this.pokemonName = name;
     this.pokemonImageAddress = image;
     this.temperature = temperature;
     this.pokemonBasedOnTemp = "";
+    this.weather = "rain";
+    this.weatherPokemon = this.checkWeather();
   }
 
 
@@ -42,7 +44,49 @@ checkTemperature(temperature) {
 
 
   }
+checkWeather(weather){
 
+  switch(weather){
+      case "tornado":
+        this.weatherPokemon = this.result.results[640].name;
+        break;
+      case "thunderstorm":
+        this.weatherPokemon = this.result.results[144].name;
+        break;
+      case "hail":
+        this.weatherPokemon = this.result.results[364].name;
+        break;
+      case "partly-cloudly-night":
+        this.weatherPokemon = this.result.results[683].name;
+        break;
+      case "partly-cloudly-day":
+        this.weatherPokemon = this.result.results[333].name;
+        break;
+      case "cloudy":
+        this.weatherPokemon = this.result.results[322].name;
+        break;
+      case "fog":
+        this.weatherPokemon = this.result.results[91].name;
+        break;
+      case "wind":
+        this.weatherPokemon = this.result.results[15].name;
+        break;
+      case "sleet":
+        this.weatherPokemon =  this.result.results[143].name;
+        break;
+      case "snow":
+        this.weatherPokemon = this.result.results[451].name;
+        break;
+      case "rain":
+        this.weatherPokemon = this.result.results[481].name;
+        break;
+
+
+  }
+  return this.weatherPokemon;
+
+
+}
 }
 
 var pokegay = new Pokemon();
